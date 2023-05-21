a855e18243428f56c6d8a73788d0bf01cbc23d57
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2023-05-21 18:43:02 -0400

    backend works for nodemailer

--- server.js
+++ server.js
@@ -1,40 +1,48 @@
+const dotenv = require("dotenv").config();
 const express = require("express");
-const app = express();
-require('dotenv').config();
-
 const bodyParser = require("body-parser") 
 const cors = require("cors")
-const nodemailer = require("nodemailer");
 
+const sendEmail = require("./utils/sendEmail")
 
-// const sendEmail = require("./utils/sendEmail")
-const password = process.env.PASSWORD;
+const app = express();
 
-//Middleware
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json());
 app.use(bodyParser.json());
 
-app.use(cors());
-
-app.post("/send_mail", cors(), async (req, res)) => {
-  let {test} = req.bodyParser
-  const transport = nodemailer.createTransport({
-    host: process.env.MAIL_HOST,
-    port: process.env.MAIL_PORT,
-    auth: {
-      user: process.env.MAIL_USER,
-      password: process.env.MAIL_PASS
-    }
-  })
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
 }
+app.use(cors(corsOptions));
+
+app.get("/", (req, res) => {
+  res.send("home page");
+});
+
+app.post("/api/sendemail", async (req, res) => {
+    const { email } = req.body;
+    try {
+      const send_to = email;
+      const sent_from = process.env.EMAIL_USER;
+      const reply_to = email;
+      const subject = "Thank You Message";
+      const message = 
+        `
+          <h3> Hi, Thank you for your message </h3>
+          <p>We are happy to hear from you and will get back to you as soon as possible.</p>
+        `;
+      await sendEmail(subject, message, send_to, sent_from, reply_to);
+      res.status(200).json({ success: true, message: "Email Sent" });
+    } catch (error) {
+      res.status(500).json(error.message);
+    }
+} )
 
 
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(
-(process.env.PORT || 5000,
-   () => {
-  console.log("Server running on port 5000");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}...`);
 })
-)
